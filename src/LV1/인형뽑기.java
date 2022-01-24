5c0045721b1657970a5d6bf7bfb455eb22d0f677
조우경 <ghvmcm123@naver.com>
2022-01-25 00:42:11 +0900

    프로그래머스

--- src/LV1/인형뽑기.java
+++ src/LV1/인형뽑기.java
@@ -1,50 +1,44 @@
 package LV1;
 
-import java.util.Scanner;
+import java.util.ArrayList;
 
 public class 인형뽑기 {
 
-	public static void main(String[] args) {
-		int[][] board = {{0,0,0,0,0,0},{0,0,0,0,0,0},{0,0,1,0,3,0},{0,2,5,0,1,0},{4,2,4,4,2,0},{3,5,1,3,1,0}};
-		//board[bord.length-1] = {n,n,n,n,n,n,n};
-		int[] moves = {1,5,3,5,1,2,1,4,1};
-		//board[n][moves[i]]�� ������ �����µ� n�� ó������ 0�� �ƴ� ĭ�� �ε�����.
-		int answer = 0;
-		//�����̱⸦ �ϸ鼭 ����� ������ ������
-		
-		
-		//board[n][moves[i]]�� ���� 0�� �ƴҰ�� success[j]�� ������ �ִµ�
-		//���� board[n][moves[i]] == sucess[j-1]�� ��� j-=2; & answer+=2;
-		//�ֳ��ϸ� j++�̶�� for���ȿ� �����ϱ�
-		//�װ� �ƴ϶�� success[j] = board[n][moves[i]] �ϰ� board[n][moves[i]]=0;
-		
-		int[] success = new int[moves.length];//������ ��� ������ ���� �迭�̴�.
-		
-		//j�� �����̱⸦ �̾Ƽ� ���� ���� ����
-		for(int i =0,j=0;i<moves.length;i++,j++) {
-			int b = 0;//���° ���� 0�� �ƴ� ���ڰ� �ִ°� ã�°�
-			while(b<board.length) {
-				if(board[b][moves[i]-1]!=0) {
-					break;
-				}
-				b++;
-			}
-			if(b<board.length) {//������ �ִ� ���
-				if(j>0&&board[b][moves[i]-1]==success[j-1]) {
-					j-=2;
-					answer+=2;
-				}else {
-					success[j] = board[b][moves[i]-1];
-				}
-				board[b][moves[i]-1]=0;
-			}else {//������ ���� ���
-				j--;
-			}
-		}
-		System.out.println(answer);
-		
-		
+   public static void main(String[] args) {
+      int[][] board = {{0,0,0,0,0,0},{0,0,0,0,0,0},{0,0,1,0,3,0},{0,2,5,0,1,0},{4,2,4,4,2,0},{3,5,1,3,1,0}};
+      int[] moves = {1,5,3,5,1,2,1,4,1};
+      
+      
+      int answer = 0;
+      ArrayList<Integer> success = new ArrayList<>();
+      
+      for(int i =0;i<moves.length;i++) {
+         
+         int b = 0;//몇번째 층에 0이 아닌 숫자가 있는가 찾는거
+         while(b<board.length) {
+            if(board[b][moves[i]-1]!=0) {
+               break;
+            }
+            b++;
+         }
+         
+         if(b<board.length) {//인형이 있는 경우
+            if(success.size()>0&&board[b][moves[i]-1]==success.get(success.size()-1)) {
+               answer+=2;
+               success.remove(success.size()-1);
+            }else {
+               success.add(board[b][moves[i]-1]);
+            }
+            
+            board[b][moves[i]-1]=0;
+         }
+      }
+      
+      
+      System.out.println(answer);
+      
+      
 
-	}
+   }
 
 }
