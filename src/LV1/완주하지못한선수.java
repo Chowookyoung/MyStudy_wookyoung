efb0bc0668609bd579c994e5caeec5d312cfeb03
조우경 <ghvmcm123@naver.com>
2022-01-27 21:25:58 +0900

    프로그래머스

---
+++ src/LV1/완주하지못한선수.java
@@ -0,0 +1,48 @@
+package LV1;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+public class 완주하지못한선수 {
+
+	public static void main(String[] args) {
+		
+		String[] participant = {"mislav", "stanko", "mislav", "ana"};
+		String[] completion = {"stanko", "ana", "mislav"};
+
+		HashSet setPart = new HashSet();
+	
+		
+//		ArrayList<String> parti = new ArrayList();
+//		for(String i:participant) {
+//			parti.add(i);
+//		}
+//		for(String i:completion) {
+//			int j = 0;
+//			while(j<parti.size()) {
+//				if(parti.get(j).equals(i)) {
+//					parti.remove(j);
+//					break;
+//				}
+//				j++;
+//			}
+//		}
+//		
+//		String answer = parti.get(0);
+
+//		for(String i:participant) {
+//			answer+=(i);
+//		}
+//		
+//		for(String i:completion) {
+//			answer = answer.replaceFirst(i, "");
+//		}
+		
+		System.out.println(answer);
+		
+
+	}
+
+}
