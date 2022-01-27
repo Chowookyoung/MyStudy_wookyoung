81b7e90da9d80d391d22b088bfd5be7cc28a3283
조우경 <ghvmcm123@naver.com>
2022-01-27 21:46:21 +0900

    프로그래머스

--- src/LV1/완주하지못한선수.java
+++ src/LV1/완주하지못한선수.java
@@ -12,9 +12,23 @@ public class 완주하지못한선수 {
 		String[] participant = {"mislav", "stanko", "mislav", "ana"};
 		String[] completion = {"stanko", "ana", "mislav"};
 
-		HashSet setPart = new HashSet();
-	
+		String answer = "";
 		
+		//정확성 성공, 효율성 실패
+//		for(String i:participant) {
+//			int j = 0;
+//			while(j<completion.length) {
+//				if(completion[j]!=null&&completion[j].equals(i)) {
+//					completion[j]=null;
+//					break;
+//				}
+//				j++;
+//			}
+//			if(j==completion.length)
+//				answer+=i;
+//		}
+		
+		//정확성 통과, 효율성 실패
 //		ArrayList<String> parti = new ArrayList();
 //		for(String i:participant) {
 //			parti.add(i);
