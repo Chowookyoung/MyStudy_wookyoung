28e79b2b180d594ba769e360d94f3fc73591b751
조우경 <ghvmcm123@naver.com>
2022-01-30 21:53:17 +0900

    lv2 오픈채팅방

---
+++ src/LV2/멀쩡한사각형.java
@@ -0,0 +1,13 @@
+package LV2;
+
+public class 멀쩡한사각형 {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+
+	}
+
+}
--- src/LV2/오픈채팅방.java
+++ src/LV2/오픈채팅방.java
@@ -8,18 +8,42 @@ public class 오픈채팅방 {
 		String[] record = {"Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"};
 		Solution sol = new Solution();
 		String[] answer = sol.solution(record);
+		for(String i : answer) {
+			System.out.println(i);
+		}
 	}
 }
 
 class Solution{
 	public String[] solution(String[] record) {
-		String[] answer = {};
-		HashMap<String,String> set = new HashMap<String,String>(record.length);
-		for(String i : record) {
-			
-			
-		}
 		
+		HashMap<String,String> map = new HashMap<String,String>(record.length);
+		int j =0;
+		int len = 0;
+		for(String i : record) {
+			String[] re = i.split(" ");
+			if(re.length==3) {
+				map.put(re[1], re[2]);
+				if(re[0].equals("Enter")) {
+					len++;
+				}
+			}else {
+				len++;
+			}
+			j++;
+		}
+		String[] answer = new String[len];
+		j=0;
+		for(String i : record) {
+			String[] re = i.split(" ");
+			if(re[0].equals("Enter")) {
+				answer[j] = map.get(re[1])+"님이 들어왔습니다.";
+				j++;
+			}else if(re[0].equals("Leave")){
+				answer[j] = map.get(re[1])+"님이 나갔습니다.";
+				j++;
+			}
+		}
 		
 		return answer;
 	}
