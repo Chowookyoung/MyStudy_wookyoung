4cbad0fbe3862f24307c3ffc0190c7f60e30df2e
조우경 <ghvmcm123@naver.com>
2022-01-31 00:03:00 +0900

    LV2. 다음에 재도전....

--- src/LV2/멀쩡한사각형.java
+++ src/LV2/멀쩡한사각형.java
@@ -23,16 +23,26 @@ class Solution1 {
         long min = Math.min(w, h);
         long max = Math.max(w, h);
         
-        if(max%min==0) {
-        	//둘 중 하나의 수가 다른 수의 배수일때
-        	answer = w*h - max;
-        }else if(min%2!=0||max%2!=0) {
-        	//둘 중 하나라도 홀 수 일때
-        	answer = w*h - (min + max - 1);
-        }else {
-        	//둘 다 짝수일때
-        	answer = (w*h) - (min*2);
+//        if(max%min==0) {
+//        	//둘 중 하나의 수가 다른 수의 배수일때
+//        	answer = w*h - max;
+//        }else if(min%2!=0||max%2!=0) {
+//        	//둘 중 하나라도 홀 수 일때
+//        	answer = w*h - (min + max - 1);
+//        }else if(min%2==0&&max%2==0) {
+//        	//둘 다 짝수일때
+//        	answer = (w*h) - (min*2);
+//        }else {
+        long gcd=min;
+        while(gcd>1) {
+        	if(w%gcd==0&&h%gcd==0) {
+        		
+        		break;
+        	}
+        	gcd--;
         }
+        System.out.println(gcd);
+        answer = w*h - (w+h-gcd);
         
         return answer;
     }
