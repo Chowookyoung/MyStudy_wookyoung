990cf55929453530cb50c947a48dd12da3abd4ad
조우경 <ghvmcm123@naver.com>
2022-01-30 20:59:22 +0900

    lv2 오픈채팅방

--- src/LV2/오픈채팅방.java
+++ src/LV2/오픈채팅방.java
@@ -1,10 +1,26 @@
 package LV2;
 
+import java.util.HashMap;
+
 public class 오픈채팅방 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String[] record = {"Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"};
+		Solution sol = new Solution();
+		String[] answer = sol.solution(record);
 	}
+}
 
+class Solution{
+	public String[] solution(String[] record) {
+		String[] answer = {};
+		HashMap<String,String> set = new HashMap<String,String>(record.length);
+		for(String i : record) {
+			
+			
+		}
+		
+		
+		return answer;
+	}
 }
