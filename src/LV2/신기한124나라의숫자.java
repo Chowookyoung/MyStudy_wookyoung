13e6f67be1277ddab8713fe1fd681c0a68750cb0
조우경 <ghvmcm123@naver.com>
2022-02-03 15:42:59 +0900

    Lv.2 124나라의 숫자

---
+++ src/LV2/신기한124나라의숫자.java
@@ -0,0 +1,23 @@
+package LV2;
+
+import java.util.Scanner;
+
+public class 신기한124나라의숫자 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		Solution3 sol = new Solution3();
+		System.out.println(sol.solution(n));
+		
+	}
+
+}
+class Solution3 {
+    public String solution(int n) {
+        String answer = "";
+        return answer;
+    }
+}
